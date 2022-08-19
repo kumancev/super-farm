3135c505fc5dabe05e1ba1380d029ad0f52d93aa
kumancev <kumancev1@gmail.com>
2022-08-19 22:21:02 +0300

    add frontend our smart-contracts

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+import NavBar from "./NavBar";
+import MainCard from "./MainCard";
+
+import { useContract } from "../context/ContractContext"; 
+
+
+const Container = styled.div`
+    height: 100%;
+    width: 100%;
+    background-color: #2b2e35;
+`;
+
+export default function Main() {
+    
+    return(
+        <>
+        <NavBar />
+        <Container>
+            <MainCard />
+        </Container>
+        </>
+    )
+}
